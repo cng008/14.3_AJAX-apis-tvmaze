af15017f75735372cde0730c9e705fd157d528aa
cng008 <92824407cng008@users.noreply.github.com>
2022-01-09 00:22:40 -0800

    list episodes of show on button click

--- tvmaze.js
+++ tvmaze.js
@@ -11,7 +11,9 @@ async function searchShows(query) {
     return {
       id: show.id,
       name: show.name,
+      genre: show.genres[0] ? show.genres[0] : '',
       summary: show.summary,
+      url: show.url,
       image: show.image ? show.image.medium : MISSING_IMAGE_URL
     };
   });
@@ -29,18 +31,18 @@ function populateShows(shows) {
   for (let show of shows) {
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
-           <div class="card-body">
-            <img class="card-img-top" src="${show.image}"></img>
-            <h5 class="card-title">${show.name}</h5>
+         <div class="card my-3" data-show-id="${show.id}">
+          <a href="${show.url}" target="_blank"><h5 class="card-header fw-bold">${show.name}</h5></a>
+           <div class="card-body bg-light">
+           <a href="${show.url}" target="_blank"><img class="card-img-top" src="${show.image}"></img></a>
+            <p class="card-text fs-5 fw-bold">${show.genre}</p>
             <p class="card-text">${show.summary}</p>
-            <a href="#" class="btn btn-primary d-grid">View Episodes</a>
+            <button class="btn btn-primary d-grid" id="get-episodes">View Episodes</button>
            </div>
          </div>
        </div>
       `
     );
-
     $showsList.append($item);
   }
 }
@@ -65,11 +67,51 @@ $('#search-form').on('submit', async function handleSearch(evt) {
 /** Given a show ID, return list of episodes:
  *      { id, name, season, number }
  */
+
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  const res = await axios.get(
-    'http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes'
-  );
-  console.log(res);
-  // TODO: return array-of-episode-info, as described in docstring above
+  let response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+
+  let episodes = response.data.map(episode => ({
+    id: episode.id,
+    name: episode.name,
+    season: episode.season,
+    number: episode.number
+  }));
+
+  return episodes;
 }
+
+/** Populate episodes list:
+ *     - given list of episodes, add espiodes to DOM
+ */
+
+function populateEpisodes(episodes) {
+  const $episodesList = $('#episodes-list');
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    let $item = $(
+      `<li>
+         ${episode.name}
+         (Season ${episode.season}, Ep. ${episode.number})
+       </li>
+      `
+    );
+
+    $episodesList.append($item);
+  }
+
+  $('#episodes-area').show();
+}
+
+/** Handle click on show name. */
+
+$('#shows-list').on(
+  'click',
+  '#get-episodes',
+  async function handleEpisodeClick(evt) {
+    let showId = $(evt.target).closest('.Show').data('show-id');
+    let episodes = await getEpisodes(showId);
+    populateEpisodes(episodes);
+  }
+);
